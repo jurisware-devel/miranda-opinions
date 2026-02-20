72ab7b9661f46ba1f820ffa0c908a3eb919710a1
Stroble <Jonathan.Stroble@albanycountyny.gov>
2026-02-20 12:53:13 -0500

    Update opinion(s)

--- update-opinions.bat
+++ update-opinions.bat
@@ -1,5 +1,20 @@
+@echo off
+
 git add .
 git commit -m "Update opinion(s)"
 git push
 
-aws s3 sync . s3://opinions.jurisware.com --delete --exclude ".git/*"
+aws s3 sync . s3://opinions.jurisware.com ^
+  --delete ^
+  --exclude ".git/*" ^
+  --cache-control "public, max-age=0, must-revalidate"
+
+REM Check if CF_DISTRIBUTION_ID is set
+IF NOT "%CF_DISTRIBUTION_ID%"=="" (
+    echo Creating CloudFront invalidation...
+    aws cloudfront create-invalidation ^
+        --distribution-id "%CF_DISTRIBUTION_ID%" ^
+        --paths "/coa/*" "/*.md"
+) ELSE (
+    echo Skipping CloudFront invalidation: CF_DISTRIBUTION_ID is not set.
+)
