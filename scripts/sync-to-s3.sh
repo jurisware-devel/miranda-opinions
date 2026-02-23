1959179ebe3b1637565053477a26b78294dfadd5
Jurisware Developer <devel@jurisware.com>
2026-02-23 08:03:56 -0500

    devel/refactor

--- scripts/sync-to-s3.sh
+++ scripts/sync-to-s3.sh
@@ -1,7 +1,3 @@
 #!/bin/zsh
 
-<<<<<<< HEAD
 aws s3 sync ~/Projects/miranda-opinions s3://opinions.jurisware.com --delete --exclude ".git/*"
-=======
-aws s3 sync ~/Projects/miranda-opinions s3://opinions.jurisware.com --delete
->>>>>>> 979731796303b26932dbbf7f35e4bafb69481c67
